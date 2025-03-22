fa66e69bf6ed60da2a176ada59ac544c221d0a3c
Nitesh Pal <mssnv42@gmail.com>
2025-03-22 17:17:58 +0530

    Changes in the Home components

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,17 +1,26 @@
 import React from "react";
-import "../styles/HomePage.css"
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Login from "../Components/Login";
 import SignUp from "../Components/SignUp";
+import "../styles/HomePage.css"
+import backgroundImage from '../images/background.png';
 
 const HomePage = () => {
   return (
     <Router>
-      <div>
+      <div
+        className="container-fluid d-flex flex-column align-items-center justify-content-center vh-100"
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: "cover",
+          position: "relative",
+        }}
+        
+      >
         {/* Navbar */}
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary w-100 position-absolute top-0">
           <div className="container">
-            <Link className="navbar-brand text-white" to="/">MyApp</Link>
+            <Link className="navbar-brand text-white" style={{ fontWeight: "600", fontSize: "25px" }} to="/">TodoApp</Link>
             <button
               className="navbar-toggler"
               type="button"
@@ -26,34 +35,62 @@ const HomePage = () => {
             <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
               <ul className="navbar-nav">
                 <li className="nav-item">
-                  <Link className="nav-link btn btn-light text-primary px-4" to="/login">Login</Link>
+                  <Link className="nav-link btn-login btn btn-outline-light me-2 px-4" to="/login">Login</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link btn btn-light text-primary px-4" to="/signup">Sign Up</Link>
+                  <Link className="nav-link btn-Sign btn btn-outline-light me-2 px-4" to="/signup">Sign Up</Link>
                 </li>
               </ul>
             </div>
           </div>
         </nav>
 
-        {/* Routes */}
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/" element={
-            <div className="container text-center mt-5">
-              <div className="row justify-content-center">
-                <div className="col-md-8">
-                  <h1 className="display-4">Welcome to MyApp</h1>
-                  <p className="lead">
-                    Build amazing applications with ease. Sign up today and get started!
-                  </p>
-                  <Link className="btn btn-primary btn-lg" to="/get-started">Get Started</Link>
+          <Route path="/signup" element={<SignUp />} />          
+        </Routes>
+
+        {/* Feature Cards */}
+        <div className="container mt-5">
+          <div className="row justify-content-center">
+            <div className="col-md-3">
+              <div className="card text-center shadow-lg">
+                <img src="https://via.placeholder.com/150" className="card-img-top" alt="Increase Productivity" />
+                <div className="card-body">
+                  <h5 className="card-title">Increase Productivity</h5>
+                  <p className="card-text">Stay on top of your tasks and get more done efficiently.</p>
                 </div>
               </div>
             </div>
-          } />
-        </Routes>
+            <div className="col-md-3">
+              <div className="card text-center shadow-lg">
+                <img src="https://via.placeholder.com/150" className="card-img-top" alt="Time Management" />
+                <div className="card-body">
+                  <h5 className="card-title">Time Management</h5>
+                  <p className="card-text">Plan your day effectively and never miss a deadline.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="card text-center shadow-lg">
+                <img src="https://via.placeholder.com/150" className="card-img-top" alt="Goal Tracking" />
+                <div className="card-body">
+                  <h5 className="card-title">Goal Tracking</h5>
+                  <p className="card-text">Set, track, and achieve your goals with ease.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="card text-center shadow-lg">
+                <img src="https://via.placeholder.com/150" className="card-img-top" alt="Reduce Stress" />
+                <div className="card-body">
+                  <h5 className="card-title">Reduce Stress</h5>
+                  <p className="card-text">Organize your tasks and reduce daily stress.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </Router>
   );
